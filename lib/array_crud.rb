14a5e4d96d9094e17335cefce05d8d7343968365
Katana Tran <tkatyk21@gmail.com>
2019-06-18 23:17:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,18 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1] 
+  array << 2
 end
 
 def add_element_to_start_of_array(array, element)
+  array = [1]
   
 end
 
