06f30b38e826f465fc4ebd89869300f9dd5fd264
Katana Tran <tkatyk21@gmail.com>
2019-06-18 23:23:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,27 +11,22 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [1]
-  array.unshift(2)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array =[1,2]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array =[1,2]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array =[1,2]
-  array[1]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array = [1,2]
   array[0]
 end
 
