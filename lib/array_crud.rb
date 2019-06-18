04f05be75d8cf4cb5c01365957b5ca5eed07f633
Katana Tran <tkatyk21@gmail.com>
2019-06-18 23:21:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,12 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array =[1,2]
-  array(1)
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = [1,2]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
